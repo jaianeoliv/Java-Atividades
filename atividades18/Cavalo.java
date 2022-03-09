e74118ed2fd4a9324ffd3584fe39039322cbe300
Jaiane Oliveira <99280014+jaianeoliv@users.noreply.github.com>
2022-03-09 19:07:58 -0300

    Atividade #18 - Exercícios herança/polimorfismo

---
+++ atividades18/Cavalo.java
@@ -0,0 +1,13 @@
+package br.com.generation.atividades18;
+
+public class Cavalo extends Cachorro {
+
+	public void correr() {
+		System.out.println("Cavalo correndo...");
+	}
+
+	public void emitirSom() {
+		System.out.println("Emitindo som: *relinchar* hiih in in hinir");
+	}
+	
+}
